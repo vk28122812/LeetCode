ccf3dd8797d918a1337d6a7331489f1a587672ff
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2024-10-14 20:36:55 +0530

    Time: 200 ms (35.96%) | Memory: 60.2 MB (13.26%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,map<pair<int,int>,int>&dp,int&target,int idx,int s){
+    int solve(vector<int>&nums,unordered_map<int,unordered_map<int,int>>&dp,int&target,int idx,int s){
         if(idx==nums.size()){
             return s==target;
         }
-        if(dp.find({s,idx})!= dp.end())return dp[{s,idx}];
+        if(dp.find(idx)!= dp.end() && dp[idx].find(s) != dp[idx].end())return dp[idx][s];
         int add=solve(nums,dp,target,idx+1,s+nums[idx]);
         int minus=solve(nums,dp,target,idx+1,s-nums[idx]);
-        return dp[{s,idx}]=add+minus;
+        return dp[idx][s]=add+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-        map<pair<int,int>,int> dp;
+        unordered_map<int, unordered_map<int,int>> dp;
         return solve(nums,dp,target,0,0);
     }
 };
