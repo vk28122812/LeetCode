30e58160894ea58487f3470ee2249cf55038c140
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2024-10-14 22:08:20 +0530

    Time: 236 ms (35.08%) | Memory: 183 MB (11.89%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool targetSum(vector<int>&nums,vector<vector<int>>&dp,int target,int idx,int sum){
+        if(idx==nums.size())return sum==target;
+        if(dp[idx][sum] != -1)return dp[idx][sum];
+        int op1 = targetSum(nums,dp,target,idx+1,sum);
+        int op2 = targetSum(nums,dp,target,idx+1,sum+nums[idx]);
+        return dp[idx][sum]=op1||op2;
+    }
+    bool canPartition(vector<int>& nums) {
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        if(s%2)return 0;
+        s/=2;
+        vector<vector<int>>dp(nums.size(), vector<int>(2*s+1,-1));
+        return targetSum(nums,dp,s,0,0);
+    }
+};
