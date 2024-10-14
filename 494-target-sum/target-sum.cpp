df8ce35eaa01227f79e6a60ac39283f9225c2959
VISHAL KUMAR <103324753+vk28122812@users.noreply.github.com>
2024-10-14 20:52:09 +0530

    Time: 13 ms (53.83%) | Memory: 24.6 MB (26.67%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,unordered_map<int,unordered_map<int,int>>&dp,int&target,int idx,int s){
-        if(idx==nums.size()){
-            return s==target;
-        }
-        if(dp.find(idx)!= dp.end() && dp[idx].find(s) != dp[idx].end())return dp[idx][s];
-        int add=solve(nums,dp,target,idx+1,s+nums[idx]);
-        int minus=solve(nums,dp,target,idx+1,s-nums[idx]);
-        return dp[idx][s]=add+minus;
+    int solve(vector<int>&nums,vector<vector<int>>&dp,int target,int idx,int s,int offset){
+        if(idx==nums.size()){return s==target;}
+        if(dp[idx][s+offset] != -1)return dp[idx][s+offset];
+        return dp[idx][s+offset] = solve(nums,dp,target,idx+1,s-nums[idx],offset) + solve(nums,dp,target,idx+1,s+nums[idx],offset);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-        unordered_map<int, unordered_map<int,int>> dp;
-        return solve(nums,dp,target,0,0);
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        int offset = s;
+        vector<vector<int>> dp(nums.size(), vector<int>(2*s+1,-1));
+        return solve(nums,dp,target,0,0,offset);
     }
 };
